6273ceb4a80003e6255fa7c2d450c5af4cab7995
Herisson Silva <herissonsilvahs@gmail.com>
2022-03-01 00:30:41 -0300

    chore: work with vector

--- src/currency/mod.rs
+++ src/currency/mod.rs
@@ -11,3 +11,11 @@ pub fn fiat_symbol(fiat: Fiat) -> String {
       Fiat::Pounds => String::from("£")
   }
 }
+
+pub fn get_currency_list() -> Vec<String> {
+  let mut currencies: Vec<String> = Vec::new();
+  currencies.push(String::from("BRL"));
+  currencies.push(String::from("USD"));
+  currencies.push(String::from("GBP"));
+  currencies
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 mod my_games;
 mod currency;
 
-use my_games::game;
-use currency::fiat_symbol;
-use currency::Fiat;
+// use my_games::game;
+// use currency::fiat_symbol;
+// use currency::Fiat;
+use currency::get_currency_list;
 
 
 fn main() {
@@ -14,9 +15,13 @@ fn main() {
     // let result: i32 = sum(2, 5);
     // println!("Result of sum: {}", result);
 
-    println!("Dolar: {}\nReal: {}\nLibra: {}", fiat_symbol(Fiat::Dollar), fiat_symbol(Fiat::Real), fiat_symbol(Fiat::Pounds));
+    // println!("Dolar: {}\nReal: {}\nLibra: {}", fiat_symbol(Fiat::Dollar), fiat_symbol(Fiat::Real), fiat_symbol(Fiat::Pounds));
 
-    game::guess_game();
+    // game::guess_game();
+
+    for currency_symbol in get_currency_list() {
+        println!("currency: {}", currency_symbol)
+    }
 }
 
 // fn sum(num1: i32, num2: i32) -> i32 {
